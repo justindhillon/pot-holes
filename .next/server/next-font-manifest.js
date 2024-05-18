1c5dc0b0d9a94e0778da92fdd6772887dc47c7dd
justindhillon <justin.singh.dhillon@gmail.com>
2024-05-18 15:29:17 -0700

    add basic map

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/var/home/justin/Github/pot-holes/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
